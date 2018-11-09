12d2bcdb2eba9d41809f0cdc04e3e860a586dd7a
Akbar0102 <akbar0102@student.upi.edu>
2018-11-09 12:42:09 +0700

    menambahkan tim ke dalam database

--- app/src/main/java/com/trois/android/footballclubapi/detail/TeamDetailActivity.kt
+++ app/src/main/java/com/trois/android/footballclubapi/detail/TeamDetailActivity.kt
@@ -1,11 +1,14 @@
 package com.trois.android.footballclubapi.detail
 
+import android.database.sqlite.SQLiteConstraintException
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
@@ -14,11 +17,18 @@ import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import com.trois.android.footballclubapi.R.color.colorAccent
 import com.trois.android.footballclubapi.R.color.colorPrimaryText
+import com.trois.android.footballclubapi.R.id.add_to_favorite
+import com.trois.android.footballclubapi.R.id.home
+import com.trois.android.footballclubapi.R.menu.detail_menu
 import com.trois.android.footballclubapi.api.ApiRepository
+import com.trois.android.footballclubapi.db.database
+import com.trois.android.footballclubapi.model.Favorite
 import com.trois.android.footballclubapi.model.Team
 import com.trois.android.footballclubapi.util.invisible
 import com.trois.android.footballclubapi.util.visible
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
@@ -36,6 +46,9 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
     private lateinit var presenter: TeamDetailPresenter
     private lateinit var teams: Team
     private lateinit var id: String
+
+    private var menuItem: Menu? = null
+    private var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -127,5 +140,40 @@ class TeamDetailActivity : AppCompatActivity(), TeamDetailView {
         teamDescription.text = data[0].teamDescription
         teamFormedYear.text = data[0].teamFormedYear
         teamStadium.text = data[0].teamStadium
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(detail_menu, menu)
+        menuItem = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            home -> {
+                finish()
+                true
+            }
+            add_to_favorite -> {
+                addToFavorite()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun addToFavorite(){
+        try {
+            database.use {
+                insert(Favorite.TABLE_FAVORITE,
+                        Favorite.TEAM_ID to teams.teamId,
+                        Favorite.TEAM_NAME to teams.teamName,
+                        Favorite.TEAM_BADGE to teams.teamBadge)
+            }
+            snackbar(swipeRefreshLayout, "Added to favorite").show()
+        }catch (e: SQLiteConstraintException){
+            snackbar(swipeRefreshLayout, e.localizedMessage).show()
+        }
     }
 }
